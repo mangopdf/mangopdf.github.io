4649af96047f7cc5abdb7ced09fa9a12e16212d9
Alex <defaultnamehere@gmail.com>
2017-08-30 23:15:56 +1000

    bad scrollspy gags honestly

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,23 +1,46 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
 
-  if (document.referrer != "") {
-      $("div.useragent a").text(document.referrer);
-      $("div.useragent").show();
-  }
+    if (document.referrer != "") {
+        $("div.useragent a").text(document.referrer);
+        $("div.useragent").show();
+    }
 
-  $('#hardmode').change(function() {
-      if (this.checked) {
-          window.stopEmoji = false;
-          emojiCursor();
-          $("hr.star-light, hr.star-primary").addClass("speedy");
-      }
-      else {
-          window.stopEmoji = true;
-          emojiCursor();
-          $("hr.star-light, hr.star-primary").removeClass("speedy");
-      }
-  });
+    $('#hardmode').change(function() {
+        if (this.checked) {
+            window.stopEmoji = false;
+            emojiCursor();
+            $("hr.star-light, hr.star-primary").addClass("speedy");
+        }
+        else {
+            window.stopEmoji = true;
+            emojiCursor();
+            $("hr.star-light, hr.star-primary").removeClass("speedy");
+        }
+    });
+
+    var gags = {
+        'blog': 'blongo plongos',
+        'websites': 'hypertext fever dreams',
+        'talks': 'embarrassing high-speed flubs',
+        'tweets': 'the TWEET ZONE',
+        'about': 'secret hidden ctf'
+    };
+    var os = new OnScreen({
+        tolerance: 0,
+        debounce: 100,
+        container: window
+    });
+
+    console.log(os);
+    os.on('leave', '.scrollspy', function(elem, event) {
+        var $elem = $(elem);
+        var id = $elem.attr('id');
+        if (id) {
+            $elem.text(gags[$elem.attr('id')]);
+            $elem.addClass("spooked");
+        }
+    });
 
 })(jQuery); // End of use strict
